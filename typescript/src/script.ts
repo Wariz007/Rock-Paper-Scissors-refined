da788f4134e623bda424ecb204503a7e1f9a0175
Abdulwariz <bookbodmas31@gmail.com>
2025-05-10 11:32:43 +0100

    page one wip

--- typescript/src/script.ts
+++ typescript/src/script.ts
@@ -1,5 +1,8 @@
 const rulesPageCloseBtn = document.querySelector('.rules-close-button');
 const rulesPage = document.querySelector('.rules-page');
+const gamePage = document.querySelector('.gamePage');
+
 rulesPageCloseBtn?.addEventListener('click', () => {
     rulesPage?.classList.add('hide');
+    gamePage?.classList.remove('hide');
 })
