86d8e0451f69f7c5f2b316897ac66662e656f4d3
Abdulwariz <bookbodmas31@gmail.com>
2025-05-10 11:53:21 +0100

    updated html and dist directory

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,8 @@
 "use strict";
 const rulesPageCloseBtn = document.querySelector('.rules-close-button');
 const rulesPage = document.querySelector('.rules-page');
+const gamePage = document.querySelector('.gamePage');
 rulesPageCloseBtn === null || rulesPageCloseBtn === void 0 ? void 0 : rulesPageCloseBtn.addEventListener('click', () => {
     rulesPage === null || rulesPage === void 0 ? void 0 : rulesPage.classList.add('hide');
+    gamePage === null || gamePage === void 0 ? void 0 : gamePage.classList.remove('hide');
 });
