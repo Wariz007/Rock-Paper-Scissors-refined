f29106edaf3020beb8230a1e181c28c1247c7b80
Abdulwariz <bookbodmas31@gmail.com>
2025-05-10 04:53:07 +0100

    corrected gh-pages branch

--- typescript/src/script.ts
+++
@@ -1,5 +0,0 @@
-const rulesPageCloseBtn = document.querySelector('.rules-close-button');
-const rulesPage = document.querySelector('.rules-page');
-rulesPageCloseBtn?.addEventListener('click', () => {
-    rulesPage?.classList.add('hide');
-})
